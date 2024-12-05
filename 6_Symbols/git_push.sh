1fc6e16eee205210cd46c058d766f7cb78348a01
Erdem <rifaterdemsahin@gmail.com>
2024-12-05 16:10:38 +0000

    Self Learning Project / Codespaces Push

---
+++ 6_Symbols/git_push.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+# cd 6_Symbols 
+# chmod +x 6_Symbols/git_push.sh 
+
+# Check if a commit message was provided
+if [ -z "$1" ]; then
+  echo "Usage: $0 <commit-message>"
+  exit 1
+fi
+
+# Assign the first argument to the commit message
+COMMIT_MESSAGE=$1
+
+# Perform Git operations
+git pull
+git add .
+git commit -m "Self Learning Project / Codespaces Push "
+git commit -m "$COMMIT_MESSAGE"
+git push
+
+#  /workspaces/OpenShiftEventRouter/6_Symbols/git_push.sh "updates"
